8d08e0c563a572e2147d607c4c277b56fa4f8027
”MarcoRomanoHub” <marcoromano270@gmail.com>
2024-02-29 09:39:47 +0100

    [add] check validation

--- script.js
+++ script.js
@@ -31,8 +31,34 @@ let somma;
 let risultato;
 let vincitore;
 
-pariDispari = prompt('Scegli "pari" o "dispari"');
-numGiocatore = parseInt(prompt('Scegli un numero da 1 a 5'));
+let validPariDispari = false;
+let validNumber = false;
+
+while (!validPariDispari) {
+  pariDispari = prompt('Scrivi "pari" oppure "dispari"')
+  // verifico che la stringa inserita sia 'pari' o 'dispari'
+  if(pariDispari.toLowerCase() === 'pari' || pariDispari.toLowerCase() === 'dispari'){
+    // se la stringa è valita interrompo il ciclo quindi non chiedo un altrpo inserimento
+  validPariDispari = true;
+  }else{
+    alert('Attenzione! Devi scrivere la parola "pari" o "dispari"')
+  }
+}
+
+
+while(!validNumber){
+  numGiocatore = parseInt(prompt('Scrivi un numero da 1 a 5'));
+  if(isNaN(numGiocatore) === false){
+    if(numGiocatore > 0 && numGiocatore <=5){
+      validNumber = true;
+    }else{
+      alert('Attenzione! hai inserito un numero sbagliato')
+    }
+  }else{
+    alert('Attenzione! hai inserito una stringa e non un numero')
+  }
+}
+  
 numPc = getRandomNumber(1,5);
 console.log(numPc);
 somma = numGiocatore + numPc;
@@ -66,53 +92,3 @@ function EvenOrOdd(somma){
   console.log(risultato);
   return risultato;
 }
-
-
-
-// let validPariDispari = false;
-// let validNumber = false;
-
-// // fino a quando non è valido l'iserimento di pari/dispari ripropongo il prompt
-// while (!validPariDispari) {
-//   pariDispari = prompt('Scrivi "pari" oppure "dispari"')
-//   // verifico che la stringa inserita sia 'pari' o 'dispari'
-//   if(pariDispari.toLowerCase() === 'pari' || pariDispari.toLowerCase() === 'dispari'){
-//     // se la stringa è valita interrompo il ciclo quindi non chiedo un altrpo inserimento
-//     validPariDispari = true;
-//   }else{
-//     alert('Attenzione! Devi scrivere la parola "pari" o "dispari"')
-//   }
-// }
-
-// while(!validNumber){
-//   numGiocatore = parseInt(prompt('Scrivi un numero da 1 a 5'));
-//   if(isNaN(numGiocatore) === false){
-//     if(numGiocatore > 0 && numGiocatore <=5){
-//       validNumber = true;
-//     }else{
-//       alert('Attenzione! hai inserito un numero sbagliato')
-//     }
-//   }else{
-//     alert('Attenzione! hai inserito una stringa e non un numero')
-//   }
-// }
-
-
-// const numPc = Math.ceil(Math.random() * 5)
-// console.log(numPc)
-// const somma = numPc + numGiocatore;
-// console.log(somma)
-
-// let risultato, messaggio;
-
-// if((somma % 2 === 0 && pariDispari === 'pari') || (somma % 2 !== 0 && pariDispari === 'dispari')){
-//   messaggio = 'Vince il giocatore'
-// }else{
-//   messaggio = 'Vince il pc'
-// }
-
-// document.getElementById('output').innerHTML = `
-// Il giocatore ha giocato ${pariDispari} con numero ${numGiocatore} <br>
-// Il pc ha giocato ${numPc}  <br>
-// ${messaggio}
-// `
